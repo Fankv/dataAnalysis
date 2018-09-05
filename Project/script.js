69fe678df535497894f18e8dce182ac259a29bc3
Hernzee <42907034+Hernzee@users.noreply.github.com>
2018-09-06 00:14:02 +0800

    nav

--- Project/script.js
+++ Project/script.js
@@ -1,11 +1,25 @@
-
-$(function(){
-$(".navmenu").mouseover(function(){
-$(this).children("ul").show();
-})
-})
-$(function(){
-$(".navmenu").mouseout(function(){
-$(this).children("ul").hide();
-})
-})
+function showList(o) {
+    hideList("nav-submenu" + o.id);
+    document.getElementById("nav-submenu-" + o.id).classList.toggle("show");
+}
+ 
+ 
+function hideList(option) {
+    var dropdowns = document.getElementsByClassName("nav-submenu");
+     
+    for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.id != option) {
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+ 
+ 
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+        hideList("");
+    }
+}
